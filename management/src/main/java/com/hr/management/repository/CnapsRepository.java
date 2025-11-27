4c670491d9854b2a248c4ad46b5b2b06ebaa39ca
Rafanomezantsoa1 <lalainarafanomezantsoaa@gmail.com>
2025-11-27 17:16:43 +0100

    tena izy fa tsy nety le farany

--- management/src/main/java/com/hr/management/repository/CnapsRepository.java
+++ management/src/main/java/com/hr/management/repository/CnapsRepository.java
@@ -1,9 +1,10 @@
 package com.hr.management.repository;
 
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hr.management.entities.Cnaps;
-import java.util.Date;
 
 public interface CnapsRepository extends JpaRepository<Cnaps, Integer>{
     Cnaps findTopByStatusAndDateLessThanEqualOrderByDateDesc(String status, Date date);
